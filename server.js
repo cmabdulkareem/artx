bbaff48813a388e3a820dd3ae4ce2f40ef75bba0
“[Abdul <[cmabdulkareem@gmail.com]>
2026-08-18 23:26:22 +0530

    test

--- server.js
+++ server.js
@@ -6,7 +6,25 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
+  'https://artx.cdcinternational.in'
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
